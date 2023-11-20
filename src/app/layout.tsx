b5911de7b2cf720b411970131bfbefb8fb6d04a3
WerdoxDev <matin.tat85@gmail.com>
2023-11-20 18:02:14 +0100

    Updated metadata to use next metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,18 +1,23 @@
+import { Metadata } from "next";
 import "./globals.css";
+
+export const metadata: Metadata = {
+   openGraph: {
+      images: [
+         { url: "https://huginn.dev/screenshot.png", alt: "An image of the huginn website", width: 1024, height: 1024 },
+      ],
+      siteName: "Huginn",
+      type: "website",
+      title: "Huginn - A feature complete chat application",
+      url: "https://huginn.dev",
+      description: "A hobby chat application",
+   },
+};
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
    return (
       <html lang="en" className="h-full">
          <title>Huginn</title>
-         <meta property="og:image" content="https://huginn.dev/screenshot.png" />
-         <meta property="og:image:alt" content="An image of the huginn website" />
-         <meta property="og:image:width" content="1053" />
-         <meta property="og:image:height" content="984" />
-         <meta property="og:site_name" content="Huginn" />
-         <meta property="og:type" content="object" />
-         <meta property="og:title" content="Huginn - A feature complete chat application" />
-         <meta property="og:url" content="https://huginn.dev" />
-         <meta property="og:description" content="A hobby chat application" />
          <body className="h-full">{children}</body>
       </html>
    );
